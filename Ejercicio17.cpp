42897b8528af6024d817e1a8b92201b50fc1b0d2
00066819 <54406942+00066819@users.noreply.github.com>
2019-09-02 19:17:54 -0600

    Update Ejercicio17.cpp

--- Ejercicio17.cpp
+++ Ejercicio17.cpp
@@ -1,16 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   main.cpp
- * Author: uca
- *
- * Created on 2 de septiembre de 2019, 14:54
- */
-
 #include <iostream>
 using namespace std;
 
